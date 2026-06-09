b45f2d6dba0519b075fe3d53d8632395366952c4
Alan Gurlaskie <alangurlaskie@gmail.com>
2026-06-09 19:12:59 -0400

    Remove extra ports

--- src/tt_um_spi_config_reg.v
+++ src/tt_um_spi_config_reg.v
@@ -22,7 +22,6 @@
 module tt_um_spi_config_reg (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
-    inout  wire [7:0] uio,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
